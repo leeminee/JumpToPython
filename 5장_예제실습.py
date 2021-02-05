9cf1bc660b383108f78c9e377400d8da7b4dfbed
Minee <mingramer@gmail.com>
2021-02-05 17:44:59 +0900

    5장 클래스 공부중

---
+++ 5장_예제실습.py
@@ -0,0 +1,51 @@
+
+print("[예제1]")
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num):
+        self.result += num
+        return self.result
+cal1 = Calculator()
+cal2 = Calculator()
+print(cal1.add(3))
+print(cal1.add(4))
+print(cal2.add(3))
+print(cal2.add(7))
+print()
+
+"""
+[클래스와 객체]
+- 클래스(Class) : 붕어빵 틀
+        : 똑같은 무엇인가를 계속해서 만들어 낼 수 있는 설계 도면.
+- 객체(Object) : 클래스로 만든 피조물
+               : 동일한 클래스로 만든 객체들은 서로 전혀 영향을 주지 않는다.  
+"""
+
+print("[예제2]")
+print()
+
+print("[예제3]")
+print()
+
+print("[예제4]")
+print()
+
+print("[예제5]")
+print()
+
+print("[예제6]")
+print()
+
+print("[예제7]")
+print()
+
+print("[예제8]")
+print()
+
+print("[예제9]")
+print()
+
+print("[예제10]")
+print()
